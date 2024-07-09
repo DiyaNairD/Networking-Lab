b59a1b86e641077c3e440a41afea029a0ab0b72b
Diya Nair D <130387433+DiyaNairD@users.noreply.github.com>
2024-07-09 20:39:11 +0530

    Update Server.c

--- Time_Server/Server.c
+++ Time_Server/Server.c
@@ -1,13 +1,13 @@
 // server.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<time.h>
 
 #define BUFFER_SIZE 1024
 

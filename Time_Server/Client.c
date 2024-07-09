f346936df6f775083f4e3b0b3a05eb73e8463084
Diya Nair D <130387433+DiyaNairD@users.noreply.github.com>
2024-07-09 20:40:20 +0530

    Update Client.c

--- Time_Server/Client.c
+++ Time_Server/Client.c
@@ -1,4 +1,4 @@
-// time_client.c
+// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,56 +7,61 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <time.h>
 
-int main(int argc, char **argv)
-{
-    if(argc != 3)
-    {
-        printf("Usage : %s <ip> <port>\n", argv[0]);
-        exit(0);
+#define BUFFER_SIZE 1024
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Usage: %s <ip> <port>\n", argv[0]);
+        exit(1);
     }
-    
+
     char *ip = argv[1];
     int port = atoi(argv[2]);
-    
     int sockfd;
     struct sockaddr_in server_addr;
-    char buffer[1024];
     socklen_t addr_size;
+    char buffer[BUFFER_SIZE];
     int n;
-    
+    time_t start_time, end_time;
+    double rtt_ms;
+
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sockfd < 0)
-    {
+    if (sockfd < 0) {
         perror("[-] Socket error");
         exit(1);
     }
-    
+
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(ip); 
-    
-    strcpy(buffer, "time");
-    n = sendto(sockfd, buffer, strlen(buffer) + 1, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(n < 0)
-    {
-        perror("[-] Error sending data");
-        exit(1);
-    }
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    // Send request to server
+    strcpy(buffer, "What is the current time?");
+    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
     printf("[+] Request sent: %s\n", buffer);
-    
-    bzero(buffer, 1024);
+
+    // Receive response from server
     addr_size = sizeof(server_addr);
-    n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&server_addr, &addr_size);
-    if(n < 0)
-    {
+    n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &addr_size);
+    if (n < 0) {
         perror("[-] Error receiving data");
+        close(sockfd);
         exit(1);
     }
-    printf("[+] Time received: %s\n", buffer);
-    
+    buffer[n] = '\0'; // Ensure buffer is null-terminated
+
+    // Calculate round-trip time (RTT)
+    end_time = time(NULL);
+    rtt_ms = difftime(end_time, start_time) * 1000.0;
+
+    // Display received time and RTT
+    printf("[+] Time received from server: %s\n", buffer);
+    printf("    Round-trip time: %.2f milliseconds\n", rtt_ms);
+
     close(sockfd);
-    
     return 0;
 }
+
